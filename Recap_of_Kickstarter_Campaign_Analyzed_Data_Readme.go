309fc9f66c6c04e4adfa491604857b5f59244b78
rudyduvnjak <rudyduvnjak@gmail.com>
2021-09-17 21:44:07 -0400

    Assignment No.1 Initial Push Added 2 Files

---
+++ Recap_of_Kickstarter_Campaign_Analyzed_Data_Readme.go
@@ -0,0 +1,21 @@
+1. Given the provided data, what are three conclusions we can draw about Kickstarter campaigns?
+2. What are some limitations of this dataset?
+3. What are some other possible tables and/or graphs that we could create?
+
+Outcomes of Kickstarter campign success waries, based on several factors and we make following 
+conclusions:
+    -Generally most successful campaigns are launched during middle of the year around May and June.
+    -Theater and Rock have avery good success rate, with Plays outpacing all other categories by far
+     with a very good success rate in funding based on Category chart data. Although, "Film and Video"
+    have best percentage success based on data chart.
+    -Again "Plays" and "Film and Video" have a good succes rates based on Category Statistics and number
+     of successfull campaigns. "Filmand & Video" Percentage wise have the very best success rate 
+     compared just basedon "Category" Chart data.
+
+Only limtiation of this data is that some Campaigns were canceled, even though they had 100% funding. 
+Also, we are looking at the success of the campaign based only on one round Kickstarter funding. 
+More campaigns, more chances of success.
+
+We could try to correlate "Goal" amount to "success", "failed" and "canceled" rate to gain more 
+visibility why some campaigns are more successful than others. 
+It would be good to learn rate of return, and speed of return based on funding.
